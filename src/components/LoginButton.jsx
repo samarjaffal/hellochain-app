0812f28cb8862aaef0387340df428764c1c5e885
samarjaffal <samarjaffalh@gmail.com>
2022-01-22 16:02:04 -0400

    fix typo on login button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -23,7 +23,7 @@ export const Loginbutton = ({setAccount, getHellos}) => {
     return (
         <>
           <div className="text-center w-full">
-              <button className="w-9/12 lg  :w-3/12 bg-secondary text-white p-6 rounded-md mt-8 hover:bg-third transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 font-bold" onClick={connectWallet}>Conectarte con Metamask</button>
+              <button className="w-9/12 lg:w-3/12 bg-secondary text-white p-6 rounded-md mt-8 hover:bg-third transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 font-bold" onClick={connectWallet}>Conectarte con Metamask</button>
           </div>
         </>
     );
